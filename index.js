7cdd03eb1eaf78539f2d1c9b0c4c81c0095b3222
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2024-07-01 14:49:16 -0400

    Link Firebase to project

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://gamified-habit-tracker-7dfc5-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const habitsInDB = ref(database, "habits")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
+    push(habitsInDB, inputValue)
     console.log(`${inputValue} added to database`)
 })
